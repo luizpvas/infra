6bfc0e7a9cc0922b40b5dc2a77aa6fa83b9293cc
Luiz Paulo <luiz.pv9@gmail.com>
2023-01-03 15:41:20 -0300

    refactor if expression to guard

--- lib/infra/kind/nilable.ex
+++ lib/infra/kind/nilable.ex
@@ -2,12 +2,9 @@ defmodule Infra.Kind.Nilable do
   def cast(subkind) do
     subkind_cast = Infra.Kind.Factory.for(subkind)
 
-    fn value ->
-      if is_nil(value) do
-        {:ok, nil}
-      else
-        subkind_cast.(value)
-      end
+    fn
+      value when is_nil(value) -> {:ok, nil}
+      value -> subkind_cast.(value)
     end
   end
 end

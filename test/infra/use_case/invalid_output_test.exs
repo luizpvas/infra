5b99096717219c4b5090fecc20d57c0f844224b4
Luiz Paulo <luiz.pv9@gmail.com>
2023-01-04 17:57:30 -0300

    add spec for invalid output with the steps syntax

--- test/infra/use_case/invalid_output_test.exs
+++ test/infra/use_case/invalid_output_test.exs
@@ -1,7 +1,7 @@
 defmodule Infra.UseCase.InvalidOutputTest do
   use ExUnit.Case, async: true
 
-  defmodule DummyUseCase do
+  defmodule DummyUseCase1 do
     use Infra.UseCase
 
     def call!(_) do
@@ -9,10 +9,30 @@ defmodule Infra.UseCase.InvalidOutputTest do
     end
   end
 
-  describe "use case execution" do
+  defmodule DummyUseCase2 do
+    use Infra.UseCase
+
+    steps do
+      :do_something
+    end
+
+    def do_something(_) do
+      {:ok, %{foo: "bar"}}
+    end
+  end
+
+  describe "call! syntax" do
     test "raise an error if output does not have expected format" do
       assert_raise Infra.UseCase.Output.InvalidFormat, fn ->
-        DummyUseCase.call()
+        DummyUseCase1.call()
+      end
+    end
+  end
+
+  describe "steps syntax" do
+    test "raise an error if output does not have expected format" do
+      assert_raise Infra.UseCase.Output.InvalidFormat, fn ->
+        DummyUseCase2.call()
       end
     end
   end
